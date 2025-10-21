a4dad9c964ad30f0ee2ab890c65352f66f0df494
sammy2201 <bhavanish2000@gmail.com>
2025-10-21 23:23:28 +0200

    UI changes in questions pages

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,7 +1,71 @@
+"use client";
 import WithAuth from "@/components/WithAuth";
+import styles from "./Questions.module.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {
+  onboardingQuestionsSelect,
+  onboardingQuestionsTextFormat,
+} from "./questions";
+import NavBar from "@/components/navbar/Navbar";
+
+type selectionQuestion = {
+  [key: string]: string[] | undefined;
+};
 
 const QuestionsPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <NavBar />
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Business Questions</h1>
+        <div className={styles.formCard}>
+          <Form>
+            {onboardingQuestionsTextFormat.map(
+              (question: string, index: number) => {
+                return (
+                  <Form.Group
+                    key={index}
+                    className={`mb-3 ${styles.formGroup}`}
+                    controlId={`select-${index}`}>
+                    <Form.Label> {question}</Form.Label>
+                    <Form.Control type="text" size="lg" required />
+                  </Form.Group>
+                );
+              }
+            )}
+            {onboardingQuestionsSelect.map(
+              (question: selectionQuestion, index: number) => {
+                const key = Object.keys(question)[0]; // get the question text
+                return (
+                  <Form.Group
+                    key={index}
+                    controlId={`select-${index}`}
+                    className={`mb-3 ${styles.formGroup}`}>
+                    <Form.Label>{key}</Form.Label>
+                    <Form.Select aria-label={key} size="lg" required>
+                      <option value="">Open this select menu</option>
+                      {(question as selectionQuestion)[key]?.map(
+                        (option: string, index: number) => (
+                          <option key={index} value={option}>
+                            {option}
+                          </option>
+                        )
+                      )}
+                    </Form.Select>
+                  </Form.Group>
+                );
+              }
+            )}
+
+            <Button variant="primary" className={styles.btn} type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default function ProtectedCanvas() {
---
+++ app/questions/questions.ts
@@ -0,0 +1,47 @@
+export const onboardingQuestionsSelect = [
+  {
+    "How would you describe your brand’s style?": [
+      "modern",
+      "minimal",
+      "classic",
+      "bold",
+      "playful",
+      "elegant",
+      "rustic",
+    ],
+  },
+  {
+    "What emotion do you want your brand to evoke?": [
+      "trust",
+      "creativity",
+      "energy",
+      "luxury",
+      "calm",
+      "excitement",
+    ],
+  },
+  {
+    "Would you like a light, dark, or both color themes?": [
+      "light",
+      "dark",
+      "both",
+    ],
+  },
+];
+
+export const onboardingQuestionsTextFormat = [
+  "Are there any colors you definitely want to include or avoid?",
+  "What is the name of your business?",
+  "What type of business or industry do you run?",
+  "Who is your target audience?",
+  "How would you describe the personality of your brand?",
+];
+
+export const advancedQuestions = [
+  "What are the core values of your business?",
+  "What is the main purpose of your branding?",
+  "Are there any competitors or brands you admire?",
+  "Do you have any specific geographic or cultural influences?",
+  "Would you like your color palette to have accent or secondary tones?",
+  "Do you want to differentiate from your competitors' colors?",
+];
