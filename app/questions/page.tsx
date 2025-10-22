488ce9669bc675903a12e3c3c2ba9a33af3452dd
sammy2201 <bhavanish2000@gmail.com>
2025-10-22 18:51:02 +0200

    questions are loging in

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from "react";
 import WithAuth from "@/components/WithAuth";
 import styles from "./Questions.module.css";
 import Form from "react-bootstrap/Form";
@@ -15,14 +16,56 @@ type selectionQuestion = {
   [key: string]: string[] | undefined;
 };
 
+type informationType = {
+  [key: string]: string[] | any;
+};
+
 const QuestionsPage = () => {
+  const [information, setInformation] = useState<informationType>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInformation({ ...information, [e.target.name]: e.target.value });
+  };
+
+  const handleCheckChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked, value } = e.target;
+
+    if (checked) {
+      setInformation({
+        ...information,
+        [name]: information[name] ? [...information[name], value] : [value],
+      });
+    } else {
+      setInformation({
+        ...information,
+        [name]:
+          information[name]?.filter((item: string) => item !== value) || [],
+      });
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const missingAnswers = onboardingQuestionsSelect.filter((q) => {
+      const key = Object.keys(q)[0];
+      return !information[key] || information[key].length === 0;
+    });
+
+    if (missingAnswers.length > 0) {
+      alert("Please answer all the questions.");
+      return;
+    }
+
+    console.log("submitted info:", information);
+  };
+
   return (
     <>
       <NavBar />
       <div className={styles.container}>
         <h1 className={styles.heading}>Business Questions</h1>
         <div className={styles.formCard}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             {onboardingQuestionsTextFormat.map(
               (question: string, index: number) => {
                 return (
@@ -31,7 +74,14 @@ const QuestionsPage = () => {
                     className={`mb-3 ${styles.formGroup}`}
                     controlId={`select-${index}`}>
                     <Form.Label> {question}</Form.Label>
-                    <Form.Control type="text" size="lg" required />
+                    <Form.Control
+                      type="text"
+                      size="lg"
+                      name={question}
+                      value={information.question}
+                      onChange={handleChange}
+                      required
+                    />
                   </Form.Group>
                 );
               }
@@ -48,7 +98,13 @@ const QuestionsPage = () => {
 
                     {(question as selectionQuestion)[key]?.map(
                       (option: string, index: number) => (
-                        <Form.Check label={option} name={option} />
+                        <Form.Check
+                          label={option}
+                          name={key}
+                          value={option}
+                          onChange={handleCheckChange}
+                          key={index}
+                        />
                       )
                     )}
                   </Form.Group>
